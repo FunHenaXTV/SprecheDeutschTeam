eaccec5418a9249d165eb629ec47fa77ac9d000e
Dmitry Zubkov <dz2003@bk.ru>
2020-02-09 11:54:44 +0300

    Created 2nd window

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ import start
 def timer():
     global iter
     iter += 1
-    print(iter)
     if iter == 1:
-        time = root.after(5000, timer)
+        time = root.after(7500, timer)
     else:
-        canvas.create_text(20, 20, text='test')
-
+        label = canvas.create_text(300, 100, text='Choose your character', font='Ubuntu 32', fill='#fff')
+        animation = start.Hello(root, canvas, label)
+def timer_2():
+    global iter, lines
+    iter += 1
+    if iter == 1:
+        time = root.after(8700, timer_2)
+    else:
+        lines.append(canvas.create_line(0, 150, 600, 150))
+        lines.append(canvas.create_line(300, 150, 300, 600))
+        lines.append(canvas.create_line(0, 375, 600, 375))
+def click(event):
+    print(event.x, event.y)
 iter = 0
+lines = []
 root = tk.Tk()
 root.title("SprecheDeutschTeam")
 root.configure(bg='#fff', width=600, height=600)
@@ -20,4 +31,7 @@ canvas = tk.Canvas(root, bg='#fff', width=600, height=600)
 canvas.pack()
 hello = start.Hello(root, canvas)
 timer()
+iter = 0
+timer_2()
+canvas.bind('<Button-1>', click)
 root.mainloop()
--- start.py
+++ start.py
@@ -1,21 +1,27 @@
 class Hello:
-    def __init__(self, root, canvas):
+    def __init__(self, root, canvas, label_change=''):
         self.root = root
         self.canvas = canvas
-        self.label = canvas.create_text(300, 250, text='SprecheDeutsch Team', font="Verdana 30", fill='#fff')
-        self.label2 = canvas.create_text(300, 300, text='Presents', font="Verdana 30", fill='#fff')
+        self.label_change = label_change
         self.iter = 0
         self.kof = 1
         self.k = 0
         self.timer = 0
-        self.color()
+        if self.label_change == '':
+            self.label = canvas.create_text(300, 250, text='SprecheDeutsch Team', font="Verdana 30", fill='#fff')
+            self.label2 = canvas.create_text(300, 300, text='Presents', font="Verdana 30", fill='#fff')
+            self.color()
+        else:
+            self.change_color_animation()
+
+        
 
     def color(self):
         self.timer += 1
         self.iter += self.kof*1
         if self.iter == 0:
             self.canvas.itemconfig(self.label, fill='#FFF')
-            self.canvas.itemconfig(self.label2, fill='#FFF')            
+            self.canvas.itemconfig(self.label2, fill='#FFF')
         elif self.iter == 1:
             self.canvas.itemconfig(self.label, fill='#FAFAFA')
             self.canvas.itemconfig(self.label2, fill='#FAFAFA')
@@ -69,10 +75,52 @@ class Hello:
         else:
             self.root.after_cancel(self.loop)
 
-
-
+    def change_color_animation(self):
+        self.iter += self.kof*1
+        if self.iter == 0:
+            self.canvas.itemconfig(self.label_change, fill='#FFF')
+        elif self.iter == 1:
+            self.canvas.itemconfig(self.label_change, fill='#FAFAFA')
+        elif self.iter == 2:
+            self.canvas.itemconfig(self.label_change, fill='#F2F2F2')
+        elif self.iter == 3:
+            self.canvas.itemconfig(self.label_change, fill='#E6E6E6')
+        elif self.iter == 4:
+            self.canvas.itemconfig(self.label_change, fill='#D8D8D8')
+        elif self.iter == 5:
+            self.canvas.itemconfig(self.label_change, fill='#BDBDBD')
+        elif self.iter == 6:
+            self.canvas.itemconfig(self.label_change, fill='#A4A4A4')
+        elif self.iter == 7:
+            self.canvas.itemconfig(self.label_change, fill='#848484')
+        elif self.iter == 8:
+            self.canvas.itemconfig(self.label_change, fill='#6E6E6E')
+        elif self.iter == 9:
+            self.canvas.itemconfig(self.label_change, fill='#585858')
+        elif self.iter == 10:
+            self.canvas.itemconfig(self.label_change, fill='#424242')
+        elif self.iter == 11:
+            self.canvas.itemconfig(self.label_change, fill='#2E2E2E')
+        elif self.iter == 12:
+            self.canvas.itemconfig(self.label_change, fill='#1C1C1C')
+        elif self.iter == 13:
+            self.canvas.itemconfig(self.label_change, fill='#151515')
+        elif self.iter == 14:
+            self.canvas.itemconfig(self.label_change, fill='#000')
+        elif self.iter == 15:
+            self.kof = -1
+            self.k += 1
+        elif self.iter == -1:
+            self.kof = 1
+            self.k += 1
+        if self.k < 1:
+            self.loop = self.root.after(100, self.change_color_animation)
+        else:
+            self.root.after_cancel(self.loop)
 
 iter = 0
+kof = 1
+k = 0
 # root = tk.Tk()
 # root.title("SprecheDeutschTeam")
 # root.configure(bg='#fff', width=600, height=600)
